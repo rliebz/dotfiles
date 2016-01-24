a0e10cf1fac171223651f3babbe97698f9753975
rliebz <rliebz@gmail.com>
2016-01-24 08:03:31 -0500

    Use virtualenv's global variable name for path

--- zsh/configs/python.zsh
+++ zsh/configs/python.zsh
@@ -1,6 +1,5 @@
 # Configure virtualenvwrapper
 export WORKON_HOME=${HOME}/.envs
 export PROJECT_HOME=${HOME}/Projects
-wrapper_script=/usr/local/bin/virtualenvwrapper.sh
-[[ -f ${wrapper_script} ]] && source ${wrapper_script}
-unset wrapper_script
+export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
+[[ -f ${VIRTUALENVWRAPPER_SCRIPT} ]] && source ${VIRTUALENVWRAPPER_SCRIPT}
