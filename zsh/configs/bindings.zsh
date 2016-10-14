7e1c82762f4b9e7edd83351477ab30b4b4648b42
rliebz <rliebz@gmail.com>
2016-10-13 23:03:24 -0400

    Switch bindings from vi to emacs

--- zsh/configs/bindings.zsh
+++ zsh/configs/bindings.zsh
@@ -1,16 +1,5 @@
-# Set command line keys to vi mode
-bindkey -v
-bindkey "^F" vi-cmd-mode
+# Set command line keys to emacs
+bindkey -e
 
 # Add useful keybindings
-bindkey "^A" beginning-of-line
-bindkey "^E" end-of-line
-bindkey "^K" kill-line
-bindkey "^R" history-incremental-search-backward
-bindkey "^Y" yank
-bindkey "^[f" forward-word    # alt-right
-bindkey "^[b" backward-word   # alt-left
 bindkey '^[[Z' reverse-menu-complete  # shift-tab
-
-# Reduce mode change lag
-export KEYTIMEOUT=1
