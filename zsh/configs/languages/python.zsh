f8ce17230869d39463e93ec0081bfe78c15cc085
rliebz <rliebz@gmail.com>
2017-02-27 18:57:44 -0500

    Configure pew

--- zsh/configs/languages/python.zsh
+++ zsh/configs/languages/python.zsh
@@ -1,10 +1,17 @@
 # Specify new column format to avoid deprecation warning
 export PIP_FORMAT=columns
 
-# Configure virtualenvwrapper
+# Configure virtualenvs
 export WORKON_HOME=${HOME}/.envs
 export PROJECT_HOME=${HOME}/Projects
 export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
+
+# Configure pew
+if command -v pew &> /dev/null; then
+  source $(pew shell_config)
+fi
+
+# Configure virtualenvwrapper
 if [[ -f ${VIRTUALENVWRAPPER_SCRIPT} ]]; then
   source ${VIRTUALENVWRAPPER_SCRIPT}
   alias mkvirtualenv2="mkvirtualenv -p $(which python2)"
