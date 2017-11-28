4281ce0d5024ac7816e8559814b8108d734cf905
Robert Liebowitz <robert.liebowitz@capitalone.com>
2017-11-28 11:19:25 -0500

    Limit virtualenvwrapper settings

--- zsh/configs/languages/python.zsh
+++ zsh/configs/languages/python.zsh
@@ -1,18 +1,16 @@
 # Specify new column format to avoid deprecation warning
 export PIP_FORMAT=columns
 
-# Configure virtualenvs
-export WORKON_HOME=${HOME}/.envs
-export PROJECT_HOME=${HOME}/Projects
-export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
-
 # Configure pew
 if command -v pew &> /dev/null; then
   source $(pew shell_config)
 fi
 
 # Configure virtualenvwrapper
+export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
 if [[ -f ${VIRTUALENVWRAPPER_SCRIPT} ]]; then
+  export WORKON_HOME=${HOME}/.envs
+  export PROJECT_HOME=${HOME}/Projects
   source ${VIRTUALENVWRAPPER_SCRIPT}
   alias mkvirtualenv2="mkvirtualenv -p $(which python2)"
   alias mkvirtualenv3="mkvirtualenv -p $(which python3)"
