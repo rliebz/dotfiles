1dec86983ff2841e7900403f9db0bf95b1ac489a
Robert Liebowitz <rliebz@gmail.com>
2017-12-16 11:16:03 -0500

    Remove unneeded yarn config

--- zsh/configs/languages/javascript.zsh
+++ zsh/configs/languages/javascript.zsh
@@ -1,12 +1,3 @@
-# Yarn configuration
-if command -v yarn &> /dev/null; then
-  if test -e ~/.yarnrc && ! grep -q prefix ~/.yarnrc; then
-    mkdir -p /usr/local/lib/yarnpkg
-    yarn config set prefix /usr/local/lib/yarnpkg &> /dev/null
-  fi
-  export PATH="$PATH:$(yarn global bin 2> /dev/null)"
-fi
-
 # NVM Configuration
 if [[ -f /usr/local/opt/nvm/nvm.sh ]]; then
   export NVM_DIR="$HOME/.nvm"
