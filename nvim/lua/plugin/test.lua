592b528af21cf748303f97436df65201a9ee1f31
Rob Liebowitz <rliebowitz@morningconsult.com>
2021-05-18 14:25:36 -0400

    Bump vim-test settings

--- nvim/lua/plugin/test.lua
+++ nvim/lua/plugin/test.lua
@@ -1,4 +1,7 @@
-vim.api.nvim_set_keymap('n', '<leader>tn', ':TestNearest<CR>', { noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>tf', ':TestFile<CR>', { noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>ts', ':TestSuite<CR>', { noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>tl', ':TestLast<CR>', { noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>tn', ':TestNearest<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>tf', ':TestFile<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>ts', ':TestSuite<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>tl', ':TestLast<CR>', { noremap = true })
+
+vim.g['test#strategy'] = 'floaterm'
+vim.g['test#go#gotest#options'] = '-tags integration -count=1'
