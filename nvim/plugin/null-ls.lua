70eaaa2872a46a52d80b5abed3dacaf160aa8d4b
Robert Liebowitz <rliebz@gmail.com>
2022-01-04 00:41:46 -0500

    Only use prettier when enabled

--- nvim/plugin/null-ls.lua
+++ nvim/plugin/null-ls.lua
@@ -5,7 +5,23 @@ null_ls.setup({
 	on_attach = lsp.on_attach,
 	sources = {
 		null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.prettier.with({
+			condition = function(utils)
+				return utils.root_has_file({
+					".prettierrc",
+					".prettierrc.json",
+					".prettierrc.yml",
+					".prettierrc.yaml",
+					".prettierrc.json5",
+					".prettierrc.js",
+					".prettierrc.cjs",
+					".prettierrc.config.js",
+					".prettierrc.config.cjs",
+					".prettierrc.toml",
+					"node_modules/prettier",
+				})
+			end,
+		}),
 		null_ls.builtins.formatting.rustfmt,
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.diagnostics.eslint,
