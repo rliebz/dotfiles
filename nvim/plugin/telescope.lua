12aba9078a9af61d1114e17524d14cba50803d0c
Rob Liebowitz <rliebowitz@morningconsult.com>
2022-06-02 20:33:57 -0400

    Replicate former fzf settings in telescope

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -1,11 +1,42 @@
 local telescope = require("telescope")
 
+local actions = require("telescope.actions")
 telescope.setup({
 	defaults = {
+		hidden = true,
 		sorting_strategy = "ascending",
 		layout_strategy = "horizontal",
 		layout_config = {
 			prompt_position = "top",
+		},
+		mappings = {
+			i = {
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+			},
+		},
+		vimgrep_arguments = {
+			"rg",
+			-- Defaults
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			-- Overrides
+			"--hidden",
+			"-g",
+			"!.git/*",
+			"-g",
+			"!*.min.js",
+			"-g",
+			"!*.js.map",
+		},
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
 		},
 	},
 })
