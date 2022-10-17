14a6e5a5fffd16bf186b0b15c25d10b6dd364693
Robert Liebowitz <rliebz@gmail.com>
2022-10-17 04:32:40 -0400

    Remove deprecated cmp function call

--- nvim/lua/cfg/plugins.lua
+++ nvim/lua/cfg/plugins.lua
@@ -654,8 +654,7 @@ use({
 			},
 		})
 
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 		local lspconfig = require("lspconfig")
 		lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
