329215952935f0311d33323161aa8f06f72cc2ba
Robert Liebowitz <rliebz@gmail.com>
2022-12-25 01:44:23 -0500

    Fix clover path/bindings

--- nvim/lua/cfg/plugins.lua
+++ nvim/lua/cfg/plugins.lua
@@ -230,10 +230,11 @@ return {
 		end,
 	},
 	{
-		"rliebz/vim-clover",
+		"rliebz/nvim-clover",
 		keys = {
 			{ "<leader>cu", vim.cmd.CloverUp },
 			{ "<leader>cd", vim.cmd.CloverDown },
+			{ "<leader>ct", vim.cmd.CloverToggle },
 		},
 	},
 	"romainl/vim-cool",
