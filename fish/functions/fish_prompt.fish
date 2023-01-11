29391bb35237170b465ebd32f114468ea28b9af4
Rob Liebowitz <rliebowitz@morningconsult.com>
2023-01-11 16:12:59 -0500

    Make prompt fish 3.6 compatible

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -11,9 +11,9 @@ function fish_prompt --description 'Write out the prompt'
     end
 
     # Make VCS display more fun
-    set -g __fish_git_prompt_showcolorhints
-    set -g __fish_git_prompt_showdirtystate
-    set -g __fish_git_prompt_showuntrackedfiles
+    set -g __fish_git_prompt_showcolorhints 1
+    set -g __fish_git_prompt_showdirtystate 1
+    set -g __fish_git_prompt_showuntrackedfiles 1
 
     # Write pipestatus
     set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
