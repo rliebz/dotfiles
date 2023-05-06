28f4e1cc373414cb06606904cf4060e080ccb041
Rob Liebowitz <rliebowitz@morningconsult.com>
2023-05-06 19:50:26 -0400

    Use the openapi file type more liberally

--- nvim/ftdetect/openapi.lua
+++ nvim/ftdetect/openapi.lua
@@ -1,9 +1,9 @@
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	pattern = {
-		"openapi.json",
-		"openapi.yaml",
-		"swagger.json",
-		"swagger.yaml",
+		"openapi*.json",
+		"openapi*.yaml",
+		"swagger*.json",
+		"swagger*.yaml",
 	},
 	callback = function()
 		if not vim.bo.filetype:find("openapi") then
