11c2c8c8b32cc97fd58379e6c6f52e979ef6134f
Robert Liebowitz <rliebz@gmail.com>
2023-06-28 00:31:35 -0400

    Swap from neo-tree to oil
    It took only a couple hours to run into a pretty critical bug for my
    intended workflow: the neotest summary pane was being hijacked by
    neo-tree. THe fact that there was already an issue conflicting with
    Dirvish means that it's at least as likely to be a neotest issue as a
    neo-tree issue, but it does seem a lot simpler than neo-tree, so we're
    going to roll with oil for a minute and see if that doesn't make me
    happy.

---
+++ nvim/lua/cfg/plugins/oil.lua
@@ -0,0 +1,22 @@
+return {
+	"stevearc/oil.nvim",
+	init = function()
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+		vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+	end,
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+		skip_confirm_for_simple_edits = true,
+		keymaps = {
+			["g."] = false,
+			["<C-t>"] = false,
+			["<C-p>"] = false,
+			["<C-c>"] = false,
+			["<C-h>"] = false,
+			["<C-s>"] = false,
+		},
+	},
+}
