3d820abb2ef39321c82251911a692569fd3f21cd
Robert Liebowitz <rliebz@gmail.com>
2023-08-16 07:50:36 -0400

    Add experimental neotest setup

--- nvim/lua/cfg/plugins/test.lua
+++ nvim/lua/cfg/plugins/test.lua
@@ -1,11 +1,5 @@
 return {
 	"vim-test/vim-test",
-	keys = {
-		{ "<leader>tn", vim.cmd.TestNearest },
-		{ "<leader>tf", vim.cmd.TestFile },
-		{ "<leader>ts", vim.cmd.TestSuite },
-		{ "<leader>tl", vim.cmd.TestLast },
-	},
 	config = function()
 		vim.g["test#go#gotest#options"] = {
 			all = "-count 1",
