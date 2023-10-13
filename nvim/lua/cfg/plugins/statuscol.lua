2043580cf4e62aaaeb198da710f0decccfda06e5
Robert Liebowitz <rliebz@gmail.com>
2023-10-12 22:34:32 -0400

    Add statuscol for gitsigns

---
+++ nvim/lua/cfg/plugins/statuscol.lua
@@ -0,0 +1,34 @@
+return {
+	"luukvbaal/statuscol.nvim",
+	config = function()
+		local builtin = require("statuscol.builtin")
+		require("statuscol").setup({
+			segments = {
+				{
+					sign = { name = { ".*" } },
+					click = "v:lua.ScSa",
+				},
+				{
+					text = { builtin.lnumfunc },
+					condition = { true, builtin.not_empty },
+					click = "v:lua.ScLa",
+				},
+				{
+					sign = {
+						namespace = { "gitsigns" },
+						colwidth = 1,
+						wrap = true,
+					},
+					click = "v:lua.ScSa",
+				},
+				{
+					text = { " " },
+				},
+				{
+					text = { builtin.foldfunc },
+					click = "v:lua.ScFa",
+				},
+			},
+		})
+	end,
+}
