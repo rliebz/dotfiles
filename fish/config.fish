6a0ef78d2a854e49cf7968fe0118275c9d144a7b
Rob Liebowitz <rliebowitz@morningconsult.com>
2024-03-22 10:56:49 -0400

    Move editor check below path manipulation
    Otherwise neovim never ends up on the path when we check for it.

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,5 @@
 set -g fish_greeting
 
-# Editor defaults
-if command -sq nvim
-    set -gx EDITOR nvim
-    set -gx VISUAL nvim
-    set -gx MANPAGER 'nvim +Man!'
-end
-
-# Pager defaults
-set -gx PAGER less
-set -gx LESS '-F -g -i -M -R -S -w -X -z-4'
-
-# Fast upward directory navigation
-function __multicd
-    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
-end
-abbr --add dotdot --regex '^\.\.+$' --function __multicd
-
 # XDG bin directory
 fish_add_path -P $HOME/.local/bin
 
@@ -41,8 +24,25 @@ fish_add_path -P $CARGO_HOME/bin
 
 # ASDF
 #
-# We want this last because it puts asdf at the front of our path
+# We want this last among paths because it puts asdf at the front
 set -gx ASDF_CONFIG_FILE $HOME/.config/asdf/.asdfrc
 set -gx ASDF_DATA_DIR $HOME/.local/share/asdf
 set -gx ASDF_GOLANG_MOD_VERSION_ENABLED false
 source $HOME/.dotfiles/asdf/asdf.fish
+
+# Editor defaults
+if command -sq nvim
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim
+    set -gx MANPAGER 'nvim +Man!'
+end
+
+# Pager defaults
+set -gx PAGER less
+set -gx LESS '-F -g -i -M -R -S -w -X -z-4'
+
+# Fast upward directory navigation
+function __multicd
+    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+end
+abbr --add dotdot --regex '^\.\.+$' --function __multicd
