1b70916d62b40b8d77f90e76070d07111503d87d
Robert Liebowitz <rliebz@gmail.com>
2024-04-07 08:13:41 -0400

    Disable lua_ls formatting in favor of stylua

--- nvim/lua/cfg/plugins/lspconfig.lua
+++ nvim/lua/cfg/plugins/lspconfig.lua
@@ -113,6 +113,10 @@ return {
 				},
 			},
 			lua_ls = {
+				on_attach = function(client)
+					client.server_capabilities.documentFormattingProvider = false
+					client.server_capabilities.documentRangeFormattingProvider = false
+				end,
 				settings = {
 					Lua = {
 						completion = {
