1e24a0090ffc3aa6b7c49a33c6d55d9c4f36b6b1
Robert Liebowitz <rliebz@gmail.com>
2024-12-27 18:00:38 -0500

    Fix snacks loading

--- nvim/lua/cfg/plugins/snacks.lua
+++ nvim/lua/cfg/plugins/snacks.lua
@@ -1,5 +1,7 @@
 return {
 	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
 	keys = {
 		{ "<c-/>", function() require("snacks").terminal() end, desc = "Toggle Terminal", mode = { "n", "t" } },
 	},
