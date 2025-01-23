5d0b1a43b0dcba6c1300576f15e577d942b85a42
Rob Liebowitz <rliebowitz@morningconsult.com>
2025-01-23 10:37:30 -0500

    Remove keymap for deprecated gitsigns function

--- nvim/lua/cfg/plugins/gitsigns.lua
+++ nvim/lua/cfg/plugins/gitsigns.lua
@@ -42,7 +42,6 @@ return {
 				function() gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end
 			)
 			map("n", "<leader>hS", gitsigns.stage_buffer)
-			map("n", "<leader>hu", gitsigns.undo_stage_hunk)
 			map("n", "<leader>hR", gitsigns.reset_buffer)
 			map("n", "<leader>hp", gitsigns.preview_hunk)
 			map("n", "<leader>hb", function() gitsigns.blame_line({ full = true }) end)
