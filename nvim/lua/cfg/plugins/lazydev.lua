d861849d669a89ffef35d127ec134404e44ca5ab
Robert Liebowitz <rliebz@gmail.com>
2025-05-12 00:30:10 -0400

    Drop luvit-meta dependency

--- nvim/lua/cfg/plugins/lazydev.lua
+++ nvim/lua/cfg/plugins/lazydev.lua
@@ -1,12 +1,9 @@
 return {
 	"folke/lazydev.nvim",
-	dependencies = {
-		{ "Bilal2453/luvit-meta", lazy = true },
-	},
 	ft = "lua",
 	opts = {
 		library = {
-			{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 			{ path = "snacks.nvim", words = { "Snacks" } },
 		},
 	},
