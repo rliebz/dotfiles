779e840906e6bf444d9658a7bbac695cd9a58f20
Rob Liebowitz <rliebowitz@morningconsult.com>
2025-05-14 10:02:25 -0400

    Enable nvimrc explicitly

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,7 @@ fish_add_path -P $HOME/.local/bin
 set -gx MISE_ACTIVATE_AGGRESSIVE true
 set -gx MISE_EXPERIMENTAL true
 set -gx MISE_DEFAULT_CONFIG_FILENAME .mise.local.toml
+set -gx MISE_IDIOMATIC_VERSION_FILE_ENABLE_TOOLS node
 set -gx MISE_GO_SET_GOBIN false
 if command -sq mise; and test -z "$MISE_SHELL"
     mise activate fish | source
