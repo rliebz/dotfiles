dddc3ccabf7ddc38c09f522ce3fadb5500b53f4e
Rob Liebowitz <rliebowitz@morningconsult.com>
2025-05-19 17:05:34 -0400

    Fix mise activation

--- fish/conf.d/mise.fish
+++ fish/conf.d/mise.fish
@@ -1,8 +1,3 @@
-set -gx MISE_ACTIVATE_AGGRESSIVE true
-set -gx MISE_EXPERIMENTAL true
-set -gx MISE_DEFAULT_CONFIG_FILENAME .mise.local.toml
-set -gx MISE_IDIOMATIC_VERSION_FILE_ENABLE_TOOLS node,python
-set -gx MISE_GO_SET_GOBIN false
-if command -sq mise; and test -z "$MISE_SHELL"
-    mise activate fish | source
-end
+# Config file source order gets awkward, and also not every install method will
+# have this, so I have to activate manually anyway.
+set -gx MISE_FISH_AUTO_ACTIVATE 0
--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,15 @@ set -gx HOMEBREW_NO_ANALYTICS 1
 set -gx HOMEBREW_NO_ENV_HINTS 1
 fish_add_path -P /opt/homebrew/bin
 
+set -gx MISE_ACTIVATE_AGGRESSIVE true
+set -gx MISE_EXPERIMENTAL true
+set -gx MISE_DEFAULT_CONFIG_FILENAME .mise.local.toml
+set -gx MISE_IDIOMATIC_VERSION_FILE_ENABLE_TOOLS node,python
+set -gx MISE_GO_SET_GOBIN false
+if command -sq mise; and test -z "$MISE_SHELL"
+    mise activate fish | source
+end
+
 # Bob
 fish_add_path -P $HOME/.local/share/bob/nvim-bin
 
