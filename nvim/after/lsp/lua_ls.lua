82d37bf6abdc82c7cb848b74e3075e86cfa0a540
Robert Liebowitz <rliebz@gmail.com>
2025-05-26 21:43:42 -0400

    Properly include lazy deps in lua_ls config
    I think based on how dynamic lazy.nvim is, some amount of scripting is
    required to get all of the directories, unless I literally want to
    enumerate every plug in I have installed in static config. While I could
    re-add lazydev and get basically this for basically free, the problem is
    lazydev does lazy-loading always, and that's annoying when you get a
    flash of errors on every new lua file. Plus the amount of config it
    actually takes to make this work seems to beat a full plug in by a
    chunky margin.
    
    This means I give up on my dream of being able to run
    `lua-language-server --check` from the CLI, but honestly I wasn't
    running it anyway considering how slow it was.

--- nvim/after/lsp/lua_ls.lua
+++ nvim/after/lsp/lua_ls.lua
@@ -1,3 +1,19 @@
+local library = {
+	"$VIMRUNTIME/lua",
+	"${3rd}/luv/library", -- Remove after nvim 0.12
+}
+
+if vim.fn.getcwd() == vim.fn.expand("~/.dotfiles") then
+	table.insert(library, "./nvim/lua")
+
+	local lazy_data_dir = "~/.local/share/nvim/lazy/"
+	for name, type in vim.fs.dir(lazy_data_dir) do
+		if type == "directory" then
+			table.insert(library, vim.fs.joinpath(lazy_data_dir, name, "lua"))
+		end
+	end
+end
+
 ---@type vim.lsp.Config
 return {
 	on_attach = function(client)
@@ -9,12 +25,11 @@ return {
 		Lua = {
 			runtime = {
 				version = "LuaJIT",
+				pathStrict = true,
 			},
 			workspace = {
-				library = {
-					"$VIMRUNTIME",
-					"${3rd}/luv/library", -- Remove after nvim 0.12
-				},
+				library = library,
+				ignore_dir = { "/lua" },
 				checkThirdParty = false,
 			},
 			completion = {
