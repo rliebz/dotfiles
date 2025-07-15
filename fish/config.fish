68d91af88ab258d31f9fa7cf3ee362f2c825203c
Rob Liebowitz <rliebowitz@morningconsult.com>
2025-07-15 16:19:25 -0400

    Fix mise activation in nested shells
    I was trying to avoid double activating mise, but (a) I don't think
    that's necessary, and (b) if I open a nested shell such as a neovim
    terminal, using an exported env var to detect activation is not actually
    an accurate mechanism for doing that.
    
    If this causes problems, next steps would be to read the script and
    decide if there's another mechanism for detection. But hopefully that
    isn't necessary.

--- fish/config.fish
+++ fish/config.fish
@@ -13,7 +13,7 @@ set -gx MISE_EXPERIMENTAL true
 set -gx MISE_DEFAULT_CONFIG_FILENAME .mise.local.toml
 set -gx MISE_IDIOMATIC_VERSION_FILE_ENABLE_TOOLS node,python
 set -gx MISE_GO_SET_GOBIN false
-if command -sq mise; and test -z "$MISE_SHELL"
+if command -sq mise
     mise activate fish | source
 end
 
