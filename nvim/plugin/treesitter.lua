e99b11dd9d0e38c0c7498cd28c724c08a185978c
Robert Liebowitz <rliebz@gmail.com>
2026-04-12 15:02:51 -0400

    Improve treesitter attaching performance
    This does a few things, including:
    
    - Getting the list of available parsers exactly once, instead of every
      time we attach to a file
    - Swap to `vim.treesitter.language.add` over `get_parser`. Pretty sure I
      pulled the old pattern off a GitHub issue somewhere, but this new
      pattern is explicitly documented as "can be used to check for
      available parsers before enabling treesitter features". That's this.
    - Pass the buffer and language into `vim.treesitter.start` explicitly.
      Could maybe avoid a race when swapping buffers quickly after opening.

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -27,26 +27,30 @@ local disable_indent = {
 	"yaml",
 }
 
+local nvim_treesitter = require("nvim-treesitter")
+local available = nvim_treesitter.get_available()
+
 vim.api.nvim_create_autocmd("FileType", {
 	callback = function(ev)
 		local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
-		if vim.list_contains(ignore_install, lang) then
+		if not lang or vim.list_contains(ignore_install, lang) then
 			return
 		end
 
-		local nvim_treesitter = require("nvim-treesitter")
-
-		local available = nvim_treesitter.get_available()
 		if not vim.list_contains(available, lang) then
 			return
 		end
 
-		nvim_treesitter.install({ lang }):wait(30000)
-		if not pcall(vim.treesitter.get_parser, ev.buf) then
+		local installed = nvim_treesitter.get_installed()
+		if not vim.list_contains(installed, lang) then
+			nvim_treesitter.install({ lang }):wait(30000)
+		end
+
+		if not vim.treesitter.language.add(lang) then
 			return
 		end
 
-		vim.treesitter.start()
+		vim.treesitter.start(ev.buf, lang)
 		vim.opt_local.foldmethod = "expr"
 		vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 		vim.opt_local.foldtext = ""
